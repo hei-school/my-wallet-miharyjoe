065cf65a34bb70181c63749542053747c52d3a59
miharyjoel <miharyjoel@gmail.com>
2023-12-09 16:51:24 +0300

    feat: add card manager

--- wallet.py
+++ wallet.py
@@ -7,15 +7,52 @@ class Transaction:
         self.status = status
 
     def __str__(self):
-        date_str = self.date.strftime("%Y-%m-%d %H:%M:%S")
-        return f"| {date_str} | {self.amount:.2f} | {self.status} |"
+        date_string = self.date.strftime("%Y-%m-%d %H:%M:%S")
+        return f"| {date_string} | {self.amount:.2f} | {self.status.ljust(15)} |"
+
+class Card:
+    def __init__(self, type, details):
+        self.type = type
+        self.details = details
+
+    def __str__(self):
+        return f"| {self.type.ljust(15)} | {self.details.ljust(40)} |"
+
+class CardManager:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, type, details):
+        card = Card(type, details)
+        self.cards.append(card)
+        print("Card added successfully.")
+
+    def display_cards(self):
+        if not self.cards:
+            print("No cards available.")
+        else:
+            print("Card Manager:")
+            print("+-----------------+----------------------------------------+")
+            print("| Type            | Details                                |")
+            print("+-----------------+----------------------------------------+")
+            for card in self.cards:
+                print(card)
+            print("+-----------------+----------------------------------------+")
+
+    def remove_card(self, index):
+        if 0 <= index < len(self.cards):
+            del self.cards[index]
+            print("Card removed successfully.")
+        else:
+            print("Invalid card index.")
 
 class Wallet:
     def __init__(self):
         self.transaction_history = []
         self.balance = 0.0
-        self.balance_dollar = 0.0
         self.balance_euro = 0.0
+        self.balance_dollar = 0.0
+        self.card_manager = CardManager()
         self.conversion_rate_to_dollar = 0.00022
         self.conversion_rate_to_euro = 0.00020
 
@@ -61,10 +98,8 @@ class Wallet:
         print("+---------------------+------------+-----------------+")
         print("| Date                | Amount     | Status          |")
         print("+---------------------+------------+-----------------+")
-
         for transaction in self.transaction_history:
-            print(str(transaction))
-
+            print(transaction)
         print("+---------------------+------------+-----------------+")
 
     def add_transaction(self, amount, status):
@@ -76,42 +111,80 @@ class Wallet:
         print(f"Current Balance Dollar: {self.balance_dollar}")
         print(f"Current Balance Euro: {self.balance_euro}")
 
+    def card_manager_menu(self):
+        while True:
+            print("""
+                    Card Manager Menu:
+                    1 - Add Card
+                    2 - Display Cards
+                    3 - Remove Card
+                    0 - Back to Main Menu
+                    """)
+
+            choice = int(input())
+            if choice == 1:
+                self.add_card()
+            elif choice == 2:
+                self.card_manager.display_cards()
+            elif choice == 3:
+                self.remove_card()
+            elif choice == 0:
+                print("Returning to the main menu.")
+                return
+            else:
+                print("Invalid choice. Please try again.")
+
+    def add_card(self):
+        input()  # Consume the newline character
+        type = input("Enter card type (e.g., National ID, Driver's License): ")
+        details = input("Enter card details: ")
+        self.card_manager.add_card(type, details)
+
+    def remove_card(self):
+        index = int(input("Enter the index of the card to remove: "))
+        self.card_manager.remove_card(index)
+
+    def run(self):
+        while True:
+            print("""
+                    Welcome to your Wallet, what I can do for You master!!
+                    Choose an option:
+                    1 - Deposit
+                    2 - Make Payment
+                    3 - Convert to Dollar
+                    4 - Convert to Euro
+                    5 - Display Transaction History
+                    6 - Display Current Balance
+                    7 - Card Manager
+                    0 - Exit
+                    """)
+
+            choice = int(input())
+            if choice == 1:
+                deposit_amount = float(input("Enter deposit amount: "))
+                self.deposit(deposit_amount)
+            elif choice == 2:
+                payment_amount = float(input("Enter payment amount: "))
+                self.make_payment(payment_amount)
+            elif choice == 3:
+                ariary_to_dollar_amount = float(input("Enter Ariary amount to convert to Dollar: "))
+                self.convert_to_dollar(ariary_to_dollar_amount)
+            elif choice == 4:
+                ariary_to_euro_amount = float(input("Enter Ariary amount to convert to Euro: "))
+                self.convert_to_euro(ariary_to_euro_amount)
+            elif choice == 5:
+                self.display_transaction_history()
+            elif choice == 6:
+                self.display_balance()
+            elif choice == 7:
+                self.card_manager_menu()
+            elif choice == 0:
+                print("Exiting. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+
 if __name__ == "__main__":
     wallet = Wallet()
-
-    while True:
-        print("""
-            Welcome to your Wallet, what can I do for you master!!
-            Choose an option:
-            1 - Deposit
-            2 - Make Payment
-            3 - Convert to Dollar
-            4 - Convert to Euro
-            5 - Display Transaction History
-            6 - Display Current Balance
-            0 - Exit
-        """)
-
-        choice = int(input("Enter your choice: "))
-
-        if choice == 1:
-            deposit_amount = float(input("Enter deposit amount: "))
-            wallet.deposit(deposit_amount)
-        elif choice == 2:
-            payment_amount = float(input("Enter payment amount: "))
-            wallet.make_payment(payment_amount)
-        elif choice == 3:
-            ariary_to_dollar_amount = float(input("Enter Ariary amount to convert to Dollar: "))
-            wallet.convert_to_dollar(ariary_to_dollar_amount)
-        elif choice == 4:
-            ariary_to_euro_amount = float(input("Enter Ariary amount to convert to Euro: "))
-            wallet.convert_to_euro(ariary_to_euro_amount)
-        elif choice == 5:
-            wallet.display_transaction_history()
-        elif choice == 6:
-            wallet.display_balance()
-        elif choice == 0:
-            print("Exiting. Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please try again.")
+    wallet.run()
